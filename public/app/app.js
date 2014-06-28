d29398e4390c9e53c4a03b936c6767168466db1d
Marco Godínez <markotom@gmail.com>
2014-06-28 11:52:18 -0500

    Se añadió buscador provisional
    TODO: parsear modelos (lemas, emblemas, etc.)

--- public/app/app.js
+++ public/app/app.js
@@ -122,7 +122,28 @@ this.App.module('Header', function (Header, App, Backbone, Marionette) {
 
   Header.Views = {};
   Header.Views.Main = Marionette.LayoutView.extend({
-    template: 'header'
+    template: 'header',
+    regions: {
+      search: '#search'
+    },
+    initialize: function () {
+      this.collection = App.request('content:entities');
+      this.collection.fetch({ cache: true });
+    },
+    onShow: function () {
+      var search = this.search;
+      this.collection.on('sync', function (collection, models) {
+        search.show(new Header.Views.Search({
+          models: models
+        }));
+      });
+    }
+  });
+  Header.Views.Search = Marionette.ItemView.extend({
+    template: 'search',
+    serializeData: function () {
+      return this.options;
+    }
   });
 });
 
